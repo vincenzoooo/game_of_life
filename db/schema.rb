da2038a6a72ad5d5ace6086e6ae04bfcfd1df597
Vincenzo Lanzieri <lanzieri.vincenzo@gmail.com>
2021-08-19 14:57:57 +0200

    Change to postgressql

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_144350) do
+ActiveRecord::Schema.define(version: 2021_08_17_134435) do
 
-  create_table "game_boards", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "game_configs", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
