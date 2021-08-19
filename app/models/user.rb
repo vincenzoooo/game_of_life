01833a9fae43eb5143da589b28dd1de3e35ebbfb
Vincenzo Lanzieri <lanzieri.vincenzo@gmail.com>
2021-08-19 15:33:18 +0200

    Layout modifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :recoverable, :rememberable,
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :validatable
 end
